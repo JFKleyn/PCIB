8f783ab8396c6d32f3e333338db90de412aeaf01
JFKleyn <101085158+JFKleyn@users.noreply.github.com>
2026-07-02 19:50:17 +0200

    Created and completed the our story section on the about page

--- src/pages/about/AboutPage.jsx
+++ src/pages/about/AboutPage.jsx
@@ -1,11 +1,15 @@
 import { Footer } from "../../components/Footer";
+import { Footer2 } from "../home/Footer2";
 import { AboutHero } from "./AboutHero";
+import { OurStory } from "./OurStory";
 
 
 export function AboutPage() {
   return (
     <>
       <AboutHero />
+      <OurStory />
+      <Footer2 />
       <Footer />
     </>
   );
