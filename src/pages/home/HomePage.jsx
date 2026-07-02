7803ad9e4cf43216b99d79ab5ff2748e31e65d53
JFKleyn <101085158+JFKleyn@users.noreply.github.com>
2026-07-02 11:52:58 +0200

    Created and completed the why choose us section

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,11 +1,13 @@
 import { Footer } from "../../components/Footer";
 import { HomeHero } from "./HomeHero";
 import "./HomePage.css";
+import { WhyChooseUs } from "./WhyChooseUs";
 
 export function HomePage() {
   return (
     <>
       <HomeHero />
+      <WhyChooseUs />
       <Footer />
     </>
   );
---
+++ src/pages/home/WhyChooseUs.jsx
@@ -0,0 +1,38 @@
+import { ShieldIcon2, TickIcon, HandshakeIcon } from "../../components/SVGIcons";
+import "./WhyChooseUs.css";
+
+export function WhyChooseUs() {
+  return (
+    <>
+      <section>
+        <div className="section-header">
+          <h2>Why Choose Us</h2>
+          <div className="header-line"></div>
+        </div>
+        <div className="why-choose-us-content">
+          <div className="why-choose-us-item">
+            <div className="svg-container">
+              <ShieldIcon2 />
+            </div>
+            <h3>Hassle-Free Claims </h3>
+            <p>We manage the entire claims process from start to finish. Our team ensures fast resolutions while you continue your business operations undisturbed.</p>
+          </div>
+          <div className="why-choose-us-item">
+            <div className="svg-container">
+              <TickIcon />
+            </div>
+            <h3>Tailored Risk Management </h3>
+            <p>No two businesses are alike. We conduct deep analysis to curate a custom insurance portfolio that matches your unique risk profile perfectly.</p>
+          </div>
+          <div className="why-choose-us-item">
+            <div className="svg-container">
+              <HandshakeIcon />
+            </div>
+            <h3>Personal Relationship </h3>
+            <p>You aren't a policy number to us. Enjoy dedicated advisors who know your name and understand your long-term goals and aspirations.</p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
