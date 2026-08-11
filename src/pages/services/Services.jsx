843d8ba1513fe4688633089a731ffd7fc46a223e
JFKleyn <101085158+JFKleyn@users.noreply.github.com>
2026-08-11 17:00:46 +0200

    fixing images on the services page

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -3,11 +3,26 @@ import { Link } from "react-router-dom";
 import { FadeIn } from "../../components/FadeIn";
 import "./Services.css";
 
+import BAO from "../../assets/BAO.webp";
+import CAI from "../../assets/CAI.webp";
+import C from "../../assets/C.webp";
+import CY from "../../assets/CY.webp";
+import R from "../../assets/R.webp";
+import D from "../../assets/D.webp";
+import PI from "../../assets/PI.webp";
+import PAP from "../../assets/PAP.webp";
+import SI from "../../assets/SI.webp";
+import SAM from "../../assets/SAM.webp";
+import WAD from "../../assets/WAD.webp";
+import CVAF from "../../assets/CVAF.webp";
+import VICS from "../../assets/VICS.webp";
+import PCIBA from "../../assets/PCIBA.webp";
+
 const services = [
   {
     id: "business-office",
     title: "Business & Office",
-    image: "src/assets/BAO.webp",
+    image: BAO,
     content: [
       "Office insurance protects your business equipment such as computers and documents, and furniture whether you work in office premises or from your home office. Policies are available to protect your business from risks such as theft, fire and water damage, even if the contents are temporarily away from the workplace.",
     ],
@@ -15,7 +30,7 @@ const services = [
   {
     id: "Commercial-&-Industrial",
     title: "Commercial & Industrial",
-    image: "src/assets/CAI.webp",
+    image: CAI,
     content: [
       "Our solutions include the insurance management of Property Management Companies. In addition to providing comprehensive buildings insurance, cover can be extended to include loss of rent or alternative accommodation expenses, previous subsidence to the property, unoccupied properties, listed properties, landlords contents, contents of common parts, employers' liability insurance where required, property owners' liability, legal expenses and tenancy disputes cover, directors and officers' liability, equipment breakdown, and lift insurance and inspection.",
     ],
@@ -23,7 +38,7 @@ const services = [
   {
     id: "construction",
     title: "Construction",
-    image: "src/assets/C.webp",
+    image: C,
     content: [
       "Designed to cover the risks associated with a construction site, be it residential, commercial, civil or mechanical. It typically includes material damage and public liability (bodily injury and/or property damage to third parties). We can also offer Builders Warranty Insurance and Owner Builders Warranty.",
     ],
@@ -31,7 +46,7 @@ const services = [
   {
     id: "cyber",
     title: "Cyber and Commercial Crime",
-    image: "src/assets/CY.webp",
+    image: CY,
     content: [
       "Unfortunately, it is an area of increasing risk with the number of incidents growing every year. There are a range of products available to protect against these exposures. However, no two policies are the same with considerable variances in policy cover. Our recommendation is to seek skilled advice in finding what is right for you.",
     ],
@@ -39,7 +54,7 @@ const services = [
   {
     id: "retail",
     title: "Retail",
-    image: "src/assets/R.webp",
+    image: R,
     content: [
       "PCIB provide a complete range of retail insurance solutions for shops of all sizes, from small corner shops to large multi-site retailers. In the fast-moving world of retail, it is vital that your insurance keeps pace with your needs and business growth.",
     ],
@@ -47,7 +62,7 @@ const services = [
   {
     id: "domestic",
     title: "Domestic",
-    image: "src/assets/D.webp",
+    image: D,
     content: [
       "Nowadays many insurers are only insuring the straightforward residential policies and are not covering those with contentious issues unless insurers specifically note your particular circumstances as an endorsement on the policy schedule, it will invariably mean that you are not fully protected. We can provide you with competitive insurance quotations from a panel of insurers offering the best product, taking into account your individual circumstances even if these are slightly unusual to give you peace of mind and great value.",
     ],
@@ -55,7 +70,7 @@ const services = [
   {
     id: "professional-indemnity",
     title: "Professional Indemnity",
-    image: "src/assets/PI.webp",
+    image: PI,
     content: [
       "This is essential cover for companies involved in advisory roles. A form of liability insurance that helps protect professional advice and service providing individuals and companies against a claim of negligence by a client. Can also cover, partly or wholly, any damages that may be awarded in such a civil lawsuit. PCIB can arrange comprehensive and competitive packages to suit your needs.",
     ],
@@ -63,7 +78,7 @@ const services = [
   {
     id: "public-and-products",
     title: "Public & Products",
-    image: "src/assets/PAP.webp",
+    image: PAP,
     content: [
       "There are numerous types of policies with specific cover that may be appropriate to your occupation. This type of insurance is an essential part of any insurance portfolio as an injury to a third party could cost millions and have severe consequences for your business. PCIB understands the many aspects of Public and Product Liability cover and can evaluate your needs, advise and recommend appropriate solutions.",
     ],
@@ -71,7 +86,7 @@ const services = [
   {
     id: "specialist-industries",
     title: "Specialist Industries",
-    image: "src/assets/SI.webp",
+    image: SI,
     content: [
       "Speak to us if you operate in a unique market or have unusual assets, we will design an insurance programme for your special needs.",
     ],
@@ -79,7 +94,7 @@ const services = [
   {
     id: "shipping-marine",
     title: "Shipping and Marine",
-    image: "src/assets/SAM.webp",
+    image: SAM,
     content: [
       "We are able to offer the complete range of services for your marine insurance needs, this includes: Full stock throughput policies, Annual declaration cargo policies, One-off specilist shipments, Hull and liability policies, Marina cover, Cover for yachts, motor boats, dinghy's and pleasure craft both individually and for yacht clubs.",
     ],
@@ -87,7 +102,7 @@ const services = [
   {
     id: "wholesale-and-distribution",
     title: "Wholesale & Distribution",
-    image: "src/assets/WAD.webp",
+    image: WAD,
     content: [
       "Insurance for wholesalers, importers, exporters, warehousing and distribution companies. PCIB strive to provide our clients with the highest quality of independent, expert advice and service to ensure that you get real value for money from your insurance arrangements.",
     ],
@@ -95,7 +110,7 @@ const services = [
   {
     id: "commercial-vehicle-and-fleet",
     title: "Commercial Vehicle and Fleet",
-    image: "src/assets/CVAF.webp",
+    image: CVAF,
     content: [
       "From small to large fleets most people overlook the main area of fleet management which is the effective and speedy handling of the motor claims that enable your business to get on with your work should an incident occur. We pride ourselves on having a team that efficiently manage your claim to cause you the least disruption.",
     ],
@@ -103,7 +118,7 @@ const services = [
   {
     id: "vehicle-insurance-credit-shortfall",
     title: "Vehicle Insurance Credit Shortfall",
-    image: "src/assets/VICS.webp",
+    image: VICS,
     content: [
       "We offer credit shortfall cover through Vapsure. The credit shortfall product is directly linked to a vehicle which is comprehensively insured. This product covers the interest owed to the finance house up to a maximum indemnity of R200,000.00.",
     ],
@@ -111,7 +126,7 @@ const services = [
   {
     id: "home-and-roadside-assistance",
     title: "Home and Roadside Assistance",
-    image: "src/assets/PCIBA.webp",
+    image: PCIBA,
     content: [
       "These services are available 24 hours a day, 7 days a week, all year round. Should you wish to opt in for this cover, kindly contact our office on 031-266 8870 and speak to any of our underwriters to have this added to your existing policy.",
     ],
